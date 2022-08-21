dd25d779a855b31caa67f8b9454d6a7ebf0ceba6
duykhoi1504 <111678054+duykhoi1504@users.noreply.github.com>
2022-08-21 19:12:47 +0700

    upload fix2

--- assets/js/main_signin.js
+++ assets/js/main_signin.js
@@ -1,15 +1,23 @@
 function videobanner() {
     const x = document.querySelector('.play')
+    // let h = `
+    //     <div class="banner">
+    // <video class="videobanner" src="/vids/signin_page_video/Sumeru Promotional Video｜Genshin Impact.mp4"
+    //                 autoplay controls loop ></video>
+    //                 <div onclick="pause()" class="pause">
+    //                 <i class=" fa-solid fa-pause"></i>
+    //                 </div>
+    //                 </div>
+    // `
     let h = `
-    <div class="banner">
-<video class="videobanner" src="/vids/signin_page_video/Sumeru Promotional Video｜Genshin Impact.mp4"
-                autoplay controls loop ></video>
-                <div onclick="pause()" class="pause">
-                <i class=" fa-solid fa-pause"></i>
-                </div>
-                </div>
-`
-    // x.classList.toggle('activevideo')  
+        <div class="banner">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/zZEpU-DbzZU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div onclick="pause()" class="pause">
+                    <i class=" fa-solid fa-pause"></i>
+                    </div>
+                    </div>
+    `
+    //     // x.classList.toggle('activevideo')  
     // x.insertAdjacentHTML("afterend", h)
 
     x.insertAdjacentHTML("afterend", h)
---
+++ assets/js/support.js
@@ -0,0 +1,5 @@
+
+function click_menu(){
+const menu= document.querySelector('.dropdown-menu');
+menu.classList.toggle('active_menu');
+}
